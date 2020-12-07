b0bc2ac0d4deded6a0d9b0a34204e8bea044a46c
Terje Urnes <terje@urnes.net>
2020-12-07 12:32:34 +0100

    v2. Getting number of files in one bucket.

--- filearcive-get-random-file-url-from-s3.py
+++ filearcive-get-random-file-url-from-s3.py
@@ -1,16 +1,23 @@
 import json
-import boto3 # <--- Importing the AWS python library
+import boto3
+
+bucketName = "filearchive.t79.it"  # <--- Specifing the name of the S3 bucket.
 
 def lambda_handler(event, context):
-    s3Client = boto3.client('s3') # <--- Create a low-level S3 client
+    s3Client = boto3.client('s3')
 
-    try:                                            #
-        bucketResponse = s3Client.list_buckets()    # <--- Trying to read from the S3 bucket
-    except Exception as e:                          #
-        raise e                                     #
+    try:
+        objectsResponse = s3Client.list_objects(Bucket=bucketName)  # <--- Changes from getting the buckets to get the files in one specific bucket.
+        if 'Contents' not in objectsResponse:                       # <--- Checking that bucket are not empty
+            return {'statusCode': 404}                              #
+
+        print(objectsResponse)
+
+    except Exception as e:
+        raise e
 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! there are '
-                           + str(len(bucketResponse["Buckets"])) + ' bukets') # <--- Adding number of buckets in the return statement.
+                           + str(len(objectsResponse['Contents'])) + ' objects') # <--- Changing from number of buckets to number of files.
     }
